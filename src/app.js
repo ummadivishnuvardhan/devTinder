7c7c3caebdb582334182a4c97d0c23b66b333b01
ummadivishnuvardhan <ummadivishnuvardhan@gmail.com>
2025-07-27 20:34:08 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const express=require("express");
 const app=express();
-app.get("/user",(req,res)=>{
-    res.send("Hello I am from user");
+app.get("/user",(req,res,next)=>{
+     console.log("I'm fromm the first Route");
+  
+    // res.send("I think I might come before second Response");
+    next();
+},
+(req,res,next)=>{
+    console.log("I'm fromm the second Route");
+    // res.send("hey I am the Response");
+    next();
+},
+(req,res,next)=>{
+    console.log("I'm fromm the third Route");
+    res.send("hey I am the Response");
 });
-app.post("/user",(req,res)=>{
-    res.send("Data succesfully saved into the dataBase from user");
-});
-app.delete("/user",(req,res)=>{
-    res.send("Data succesfully deleted from the dataBase from user");
-});
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
